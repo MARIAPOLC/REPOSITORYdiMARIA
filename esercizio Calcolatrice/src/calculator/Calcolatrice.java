fd129ee9e64fb48f14ebbccce11853120f358584
maria <maria@100.125.0.138>
2019-12-18 17:26:34 +0100

    ho creato un ramo del codice

--- esercizio Calcolatrice/src/calculator/Calcolatrice.java
+++ esercizio Calcolatrice/src/calculator/Calcolatrice.java
@@ -11,6 +11,7 @@ public class Calcolatrice {
 	public double dividi (int x, int y) {
 		System.out.println("modifica locale");
 		System.out.println ("modifica remta + conflitto");
+		System.out.println ("modifica brunch mio");
 		if (y==0)
 			return Double.MAX_VALUE;
 		return x/y;
